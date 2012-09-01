45017d92dc53ca20cebcdc13a5573f49f3901def
Matt Harris <mharris2@uw.edu>
2012-09-01 12:54:24 -0700

    forget to save the user record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
       @current_user.attach_person_record
       redirect_to profile_path
     else
-      @current_user.person = Person.create unless @current_user.person
+      @current_user.person = Person.create && @current_user.save unless @current_user.person
       redirect_to @current_user.person.update_attribute(:type, new_identity) ? profile_path : choose_identity_path
     end
   end
