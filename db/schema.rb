7f3f22b5cccc5920b71262f0715af749d1dcceaf
Matt Harris <mharris2@uw.edu>
2012-09-22 12:01:13 -0700

    [Fixes #36502433] Trying to fix frequent relogin issue

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120903232308) do
+ActiveRecord::Schema.define(:version => 20120922184742) do
 
   create_table "changes", :force => true do |t|
     t.integer  "change_loggable_id"
@@ -427,6 +427,16 @@ ActiveRecord::Schema.define(:version => 20120903232308) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "training_completions", :force => true do |t|
     t.integer  "training_id"
