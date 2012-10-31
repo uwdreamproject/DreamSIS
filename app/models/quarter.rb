f8dab63e2e32670a839e1096bf4cb850bb91b1c1
Matt Harris <mharris2@uw.edu>
2012-10-31 01:20:03 -0700

    updating high school dashboard to always show current quarter mentors
    … even when the current quarter has signups disabled.

--- app/models/quarter.rb
+++ app/models/quarter.rb
@@ -96,6 +96,11 @@ class Quarter < ActiveRecord::Base
     Quarter.find_by_abbrev(abbrev, true)
   end
   
+  # Includes the current quarter AND the quarters allowing signups.
+  def self.current_and_allowing_signups
+    [Quarter.current_quarter, Quarter.allowing_signups].flatten.uniq
+  end
+  
   # Returns true if specified date falls between this Quarter's start and end dates.
   def include?(date)
     date >= start_date && date <= end_date
