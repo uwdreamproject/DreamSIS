eff0c5d68375d3be109384ab1f6328eaee282b6f
Matt Harris <mattkharris@gmail.com>
2013-03-18 20:17:27 -0700

    creating shared-config root for config files

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,4 +1,4 @@
 # You should store your google API tracking code in RAILS_ROOT/config/google_analytics.yml.
 
-config_file_path = "#{RAILS_ROOT}/config/google_analytics.yml"
+config_file_path = File.join(ENV['SHARED_CONFIG_ROOT'] || "#{RAILS_ROOT}/config", "google_analytics.yml")
 GOOGLE_ANALYTICS_TRACKING_ID = YAML::load(ERB.new((IO.read(config_file_path))).result)[RAILS_ENV]
