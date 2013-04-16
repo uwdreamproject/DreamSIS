e56d6de8f1d6e43801fb2315312804915c493a70
Matt Harris <mattkharris@gmail.com>
2013-04-16 15:54:07 -0700

    adding customer controller and routes

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,72 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.find :all
+  
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @customers }
+    end
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @customer }
+    end
+  end
+  
+  def new
+    @customer = Customer.new
+  
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @customer }
+    end
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def create
+    @customer = Customer.new(params[:customer])
+  
+    respond_to do |format|
+      if @customer.save
+        flash[:notice] = 'Customer was successfully created.'
+        format.html { redirect_to(@customer) }
+        format.xml  { render :xml => @customer, :status => :created, :location => @customer }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @customer.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+  
+    respond_to do |format|
+      if @customer.update_attributes(params[:customer])
+        flash[:notice] = 'Customer was successfully updated.'
+        format.html { redirect_to(@customer) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @customer.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to(admin_customers_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
