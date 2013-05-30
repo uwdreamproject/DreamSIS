b038318833db33da7b50b42717a38c3127697a2d
Matt Harris <mattkharris@gmail.com>
2013-05-30 12:17:58 -0700

    Added default education levels to start

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530172634) do
+ActiveRecord::Schema.define(:version => 20130530184116) do
 
   create_table "changes", :force => true do |t|
     t.integer  "change_loggable_id"
