8bfb6ae3e03baa0cf11845fa0072717c6b99e78f
Matt Harris <mattkharris@gmail.com>
2013-09-22 23:57:28 -0700

    Removes db default on TEXT column for mysql compatibility

--- db/migrate/20130923024747_add_college_application_choice_options_to_customer.rb
+++ db/migrate/20130923024747_add_college_application_choice_options_to_customer.rb
@@ -1,6 +1,6 @@
 class AddCollegeApplicationChoiceOptionsToCustomer < ActiveRecord::Migration
   def self.up
-    add_column :customers, :college_application_choice_options, :text, :default => "Reach\nSolid\nSafety"
+    add_column :customers, :college_application_choice_options, :text
   end
 
   def self.down
