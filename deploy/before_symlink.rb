3b17f094ea5424315c033047ea4b4eaf75188530
Matt Harris <mattkharris@gmail.com>
2013-10-06 18:02:57 -0700

    Using config prefix per EY's warning

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
-run "echo 'release_path: #{release_path}/files' >> #{shared_path}/logs.log" 
-run "mkdir -p #{shared_path}/files"
-run "ln -nfs #{shared_path}/files #{release_path}/files" 
+run "echo 'release_path: #{config.release_path}/files' >> #{config.shared_path}/logs.log" 
+run "mkdir -p #{config.shared_path}/files"
+run "ln -nfs #{config.shared_path}/files #{config.release_path}/files" 
