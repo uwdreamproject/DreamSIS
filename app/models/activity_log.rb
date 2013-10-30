1685ddcea402e8ea82e2736f6f84d5003f16a06a
Matt Harris <mattkharris@gmail.com>
2013-10-29 21:55:56 -0700

    Improves week math calcs

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -14,34 +14,34 @@ class ActivityLog < CustomerScoped
 	# and week number provided (based on Monday-start weeks).
 	def self.find_or_create_by_mentor_and_week_and_year(mentor, week_number, year)
 		mentor_id = mentor.is_a?(Mentor) ? mentor.try(:id) : mentor
-		start_date = Date.commercial(year, week_number+1, 1)
-		end_date = Date.commercial(year, week_number+1, 7)
+		start_date = Date.commercial(year, week_number, 1)
+		end_date = Date.commercial(year, week_number, 7)
 		ActivityLog.find_or_create_by_mentor_id_and_start_date_and_end_date(mentor_id, start_date, end_date)
 	end
 	
 	# Finds or creates the ActivityLog for the mentor for the current week.
 	def self.current_for(mentor)
-		week_number = Date.today.beginning_of_week.strftime("%W").to_i
+		week_number = Date.today.cweek
 		ActivityLog.find_or_create_by_mentor_and_week_and_year(mentor, week_number, Date.today.year)
 	end
   
-	# Returns the week_number for this ActivityLog by returning +strftime("%W")+
-	# and adding 1 to this ActivityLog's start_date.
+	# Returns the week_number for this ActivityLog by returning +cweek+
+	# for this ActivityLog's start_date.
 	def week_number
-		start_date.strftime("%W").to_i + 1
+		start_date.cweek
 	end
 	
 	# Finds or creates an ActivityLog for the mentor for next week.
 	def next_week_log
 		next_week_start = (start_date + 1.week).beginning_of_week
-		week_number = next_week_start.strftime("%W").to_i
+		week_number = next_week_start.cweek
 		ActivityLog.find_or_create_by_mentor_and_week_and_year(mentor_id, week_number, next_week_start.year)
 	end
 
 	# Finds or creates an ActivityLog for the mentor for last week.
 	def previous_week_log
 		previous_week_start = (start_date - 1.week - 1.day).beginning_of_week
-		week_number = previous_week_start.strftime("%W").to_i + 1
+		week_number = previous_week_start.cweek
 		ActivityLog.find_or_create_by_mentor_and_week_and_year(mentor_id, week_number, previous_week_start.year)
 	end
 	
