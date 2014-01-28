86223a95194af1379da4c74c9800aede7d024999
Matt Harris <matt@dreamsis.com>
2014-01-27 22:30:30 -0800

    Allows program URL to be blank

--- app/models/program.rb
+++ app/models/program.rb
@@ -6,6 +6,6 @@ class Program < ActiveRecord::Base
   has_and_belongs_to_many :people
   default_scope :order => "title"
   
-  validates_format_of :website_url, :with => URI.regexp(%w[http https])
+  validates_format_of :website_url, :with => URI.regexp(%w[http https]), :allow_blank => true
   
 end
