3546d9e3dfe286eda4b11b6f079a6316ef121708
Matt Harris <matt@dreamsis.com>
2014-04-01 22:34:39 -0700

    Fix error with update_attribute call

--- db/migrate/20140326192359_add_post_high_school_exit_plan_to_person.rb
+++ db/migrate/20140326192359_add_post_high_school_exit_plan_to_person.rb
@@ -4,7 +4,7 @@ class AddPostHighSchoolExitPlanToPerson < ActiveRecord::Migration
 		add_column :people, :postsecondary_plan, :string
 		
 		Participant.find(:all, :conditions => { :postsecondary_goal => "Community college" }).each do |p|
-			p.update_attribute(:postsecondary_goal => "2-year college")
+			p.update_attribute(:postsecondary_goal, "2-year college")
 		end
   end
 
@@ -13,7 +13,7 @@ class AddPostHighSchoolExitPlanToPerson < ActiveRecord::Migration
 		rename_column :people, :postsecondary_goal, :plans_after_high_school
 
 		Participant.find(:all, :conditions => { :plans_after_high_school => "2-year college" }).each do |p|
-			p.update_attribute(:plans_after_high_school => "Community college")
+			p.update_attribute(:plans_after_high_school, "Community college")
 		end
   end
 end
