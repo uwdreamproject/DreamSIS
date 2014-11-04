8a59e0ed1b8e05b2661dcdff62fc1eae57670051
Matt Harris <matt@dreamsis.com>
2014-11-03 17:31:38 -0800

    Add rollbar deployment tracking

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,6 @@
-# on_app_master do
-#   run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=YOUR_PROJECT_ACCESS_TOKEN -F environment=#{config.environment} -F revision=#{config.revision}"
-# end
+on_app_master do
+  config_file_path = File.join(config.shared_path, "config", "api-keys.yml")
+  API_KEYS = YAML::load(ERB.new((IO.read(config_file_path))).result)
+  access_token = API_KEYS["rollbar"]["server_side_access_token"]
+  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{access_token} -F environment=#{config.environment} -F revision=#{config.revision}"
+end
