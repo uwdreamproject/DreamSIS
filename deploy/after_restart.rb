8ca52a88f52b14fedc0d892f98209b18fd8a33c4
Matt Harris <matt@dreamsis.com>
2014-11-03 22:25:17 -0800

    Remove ERB

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,8 @@
-require 'ERB'
 require 'YAML'
 
 on_app_master do
   config_file_path = File.join(config.shared_path, "config", "api-keys.yml")
-  API_KEYS = YAML::load(ERB.new((IO.read(config_file_path))).result)
+  API_KEYS = YAML::load(IO.read(config_file_path))
   access_token = API_KEYS["rollbar"]["server_side_access_token"]
   run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{access_token} -F environment=#{config.environment} -F revision=#{config.revision}"
 end
