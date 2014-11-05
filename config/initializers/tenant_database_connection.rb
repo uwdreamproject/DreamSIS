8499f3f811640436c0cfd392be225d2c9be5b5ec
Matt Harris <matt@dreamsis.com>
2014-11-05 09:50:18 -0800

    Prep for using tenant-custom database connections

--- config/initializers/tenant_database_connection.rb
+++ config/initializers/tenant_database_connection.rb
@@ -1,24 +1,16 @@
-module Apartment
-  module Adapters
-    class AbstractAdapter
-
-      # Replaces Apartment's default method for returning a multitenanted configuration.
-      #
-      def multi_tenantify(tenant)
-        @config.clone.tap do |config|
-          config[:database] = environmentify(tenant)
-          
-          # database: <%= ENV['RDS_DB_NAME'] %>
-          # username: <%= ENV['RDS_USERNAME'] %>
-          # password: <%= ENV['RDS_PASSWORD'] %>
-          # host: <%= ENV['RDS_HOSTNAME'] %>
-          # port: <%= ENV['RDS_PORT'] %>
-
-          config[:host] = "test.local"
-          
-        end
-      end
-
-    end
-  end
-end
+# require 'apartment/adapters/abstract_adapter'
+#
+# Apartment::Adapters::AbstractAdapter.class_eval do
+#
+#   protected
+#
+#   # Override Apartment's multi_tenantify method to inject other configuration options as needed.
+#   def multi_tenantify(tenant)
+#     @config.clone.tap do |config|
+#       config[:database] = environmentify(tenant)
+#       config[:host] = "test.local"
+#       # adjust other settings here based on Customer
+#     end
+#   end
+#
+# end
