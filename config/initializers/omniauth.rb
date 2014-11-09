eb99c82e036d64c51c8a4c35d0837a7958acd580
Mitchell Harper <mitchwharper@gmail.com>
2014-11-09 14:40:03 -0800

    Use google_oauth2 in place of shibboleth auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Dreamsis::Application.config.middleware.use OmniAuth::Builder do
   provider :facebook, omniauth_keys["facebook"]["key"], omniauth_keys["facebook"]["secret"], :secure_image_url => true
   provider :twitter, omniauth_keys["twitter"]["key"], omniauth_keys["twitter"]["secret"]
   provider :google_oauth2, omniauth_keys["google"]["key"], omniauth_keys["google"]["secret"]
-  provider :shibboleth, :extra_fields => [:"unscoped-affiliation", :entitlement, :gws_groups, :uwNetID, :uwRegID]
+  provider :google_oauth2, omniauth_keys["google"]["key"], omniauth_keys["google"]["secret"], {:hd => "uw.edu", :name => "shibboleth"}
   provider :windowslive, omniauth_keys["windowslive"]["key"], omniauth_keys["windowslive"]["secret"], :scope => 'wl.basic'
   provider :linkedin, omniauth_keys["linkedin"]["key"], omniauth_keys["linkedin"]["secret"]  
   # provider :identity #, :on_failed_registration => SessionController.call("signup")
