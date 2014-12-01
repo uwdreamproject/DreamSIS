6048e4d7af192af96f4c0fc6af62cfd5345f655e
Matt Harris <matt@dreamsis.com>
2014-12-01 15:39:24 -0800

    Converting file names to rails 3

--- app/models/rsvp_mailer.rb
+++ app/models/rsvp_mailer.rb
@@ -10,11 +10,12 @@ class RsvpMailer < ActionMailer::Base
     @time_detail = @event.time_detail(:audience => event_attendance.audience)
     @event_description = @event.description(event_attendance.audience)
     @confirmation_message = @event.event_group.confirmation_message(event_attendance.audience) if @event.event_group
-    mail(:subject =>"Thanks for registering: #{@event.name}",
-    :to => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
-    :from =>      "do-not-reply@dreamsis.com",
-    :sent_on  =>  sent_at,
-    :css => :email)
+    mail(
+      :subject    => "Thanks for registering: #{@event.name}",
+      :to         => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
+      :from       => "do-not-reply@dreamsis.com",
+      :sent_on    =>  sent_at
+    )
   end
 
   def cancel(event_attendance, sent_at = Time.now)
@@ -22,21 +23,22 @@ class RsvpMailer < ActionMailer::Base
     @event_attendance = event_attendance
     @event = @event_attendance.event
     mail(
-    :subject =>    "Registration canceled: #{@event.name}",
-    :to => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
-    :from =>      "do-not-reply@dreamsis.com",
-    :sent_on =>    sent_at,
-    :css => :email)
+      :subject    => "Registration canceled: #{@event.name}",
+      :to         => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
+      :from       => "do-not-reply@dreamsis.com",
+      :sent_on    => sent_at
+    )
   end
 
   def reminder(event_attendance, sent_at = Time.now)
     @event_attendance = event_attendance
     @event = @event_attendance.event
     mail(
-    :subject =>   "Event Reminder: #{@event.name}",
-    :to => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
-    :from  =>     "do-not-reply@dreamsis.com",
-    :sent_on =>   sent_at)
+      :subject    => "Event Reminder: #{@event.name}",
+      :to         => "#{@event_attendance.person.try(:fullname)} <#{@event_attendance.person.try(:email)}>",
+      :from       => "do-not-reply@dreamsis.com",
+      :sent_on    => sent_at
+    )
   end
 
 end
