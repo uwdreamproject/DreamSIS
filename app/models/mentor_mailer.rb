7607b4ed5d45a968267e3f44e132754070fd67dd
Matt Harris <matt@dreamsis.com>
2014-12-04 16:37:07 -0800

    Update from email address

--- app/models/mentor_mailer.rb
+++ app/models/mentor_mailer.rb
@@ -1,6 +1,6 @@
 class MentorMailer < ActionMailer::Base
 
-  ActionMailer::Base.default_url_options[:host] = "dreamsis.org"
+  ActionMailer::Base.default_url_options[:host] = "dreamsis.com"
 
   def driver(mentor, sent_at = Time.now)
     css :email
@@ -8,7 +8,7 @@ class MentorMailer < ActionMailer::Base
     @mentor = mentor
     subject     "#{Customer.name_label} Driver Conduct Agreement"
     recipients  "#{@mentor.try(:fullname)} <#{@mentor.email}>"
-    from        "do-not-reply@dreamsis.org"
+    from        "do-not-reply@dreamsis.com"
     sent_on     sent_at
   end
 end
