431573f36481947e4016b5ea7abdc691ec903161
Mitchell Harper <mitchwharper@gmail.com>
2015-01-05 20:12:11 -0800

    Update driver email syntax

--- app/models/mentor_mailer.rb
+++ app/models/mentor_mailer.rb
@@ -3,12 +3,13 @@ class MentorMailer < ActionMailer::Base
   ActionMailer::Base.default_url_options[:host] = "dreamsis.org"
 
   def driver(mentor, sent_at = Time.now)
-    css :email
 
     @mentor = mentor
-    subject     "#{Customer.name_label} Driver Conduct Agreement"
-    recipients  "#{@mentor.try(:fullname)} <#{@mentor.email}>"
-    from        "do-not-reply@dreamsis.org"
-    sent_on     sent_at
+    mail(
+      :subject =>     "#{Customer.name_label} Driver Conduct Agreement",
+      :to =>  "#{@mentor.try(:fullname)} <#{@mentor.email}>",
+      :from =>        "do-not-reply@dreamsis.org",
+      :sent_on =>     sent_at
+    )
   end
 end
