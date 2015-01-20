725deb11c76e89813fd50da1af12ea8240f4d4c7
Mitchell Harper <mitchwharper@gmail.com>
2015-01-20 14:31:26 -0800

    Revert to active record store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Dreamsis::Application.config.session_store :cookie_store, :domain => :all, :tld_length => 2
+Dreamsis::Application.config.session_store :active_record_store
 
 # ActionController::Base.session = {
 #   :key         => '_dreamsis_session',
