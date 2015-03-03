cbc3a92175492e0b7ddb3dd9511110bc9a821c2e
Matt Harris <matt@dreamsis.com>
2015-03-02 20:19:04 -0800

    Adds college stage pipeline display and persists filters in location hash

--- app/assets/javascripts/filterable.js
+++ app/assets/javascripts/filterable.js
@@ -1,17 +1,28 @@
 $( function() {
+  updateFiltersWithLocationHash()
   executeFilters()
-  $(".filter_checkbox").change(executeFilters)
+  $(".filter_checkbox").change(changeFilterCheckbox)
+  $("#stages_selector a").click(clickStageSelector) // Add this after the ajax call, too
 })
 
 function executeFilters() {
-  if( $(".filter_checkbox:checked").length === 0 ) {
+  if( $(".filter_checkbox:checked").length === 0 && $("#stages_selector [data-stage].selected").length === 0 ) {
     showAllFilterables()
   } else {
     $(".filterable").addClass("hidden")
     selection = ""
+
+    // Add rows that match the selected stages
+    $("#stages_selector [data-stage].selected").each(function( i, element) {
+      selection += "[data-stages~=" + $(this).data("stage") + "]"
+    })
+
+    // Add rows that match the selected filters
     $(".filter_checkbox:checked").each(function (i, element) {
       selection += "[data-filter-" + $(this).data("target-filter-id") + "='" + $(this).val() + "']"
     })
+
+    // Show all the rows in the selection
     $(selection).removeClass("hidden")
   }
   updateRecordCount()
@@ -43,6 +54,7 @@ function clearAllFilters() {
 // Tries to update a div with id "filtered_record_count" with the number of filterables that are visible.
 function updateRecordCount(filter_key) {
   $('#filtered_record_count').html($('.filterable:not(.hidden)').size() + " of")
+  $('#total_record_count').html($('.filterable').size())
   $(".filter_checkbox").each(function(i) {
     $(this).siblings("small").html( 
       $(".filterable:not(.hidden)[data-filter-" + $(this).data("target-filter-id") + "='true']" ).size() 
@@ -101,5 +113,77 @@ function filterByCounty(filter_value) {
     $('.filterable-by-county').hide();
     $('.filterable-by-county.county-' + filter_value).show()
     window.location.hash = "filter-county=" + filter_value
+  }
+}
+
+/*
+  Function to call when a filter checkbox is changed.
+*/
+function changeFilterCheckbox() {
+  updateLocationHashWithFilters()
+  executeFilters()
+}
+
+/*
+  Function to call when a Stage selector is clicked.
+*/
+function clickStageSelector(event) {
+  event.preventDefault()
+  $( this ).toggleClass("selected")
+  updateLocationHashWithFilters()
+  executeFilters()
+}
+
+/*
+  Updates the hash in the URL with the selected filterables and stages.
+*/
+function updateLocationHashWithFilters() {
+  var filterHash = []
+  $(".filter_checkbox:checked").each(function (i, element) {
+     filterHash.push($(this).data("target-filter-id") + "|" + $(this).val())
+  })
+  
+  var stagesHash = []
+  $("#stages_selector [data-stage].selected").each(function( i, element) {
+    stagesHash.push($(this).data("stage"))
+  })
+  
+  var newHash = []
+  if (stagesHash.length > 0)
+    newHash.push("stages=" + stagesHash.join(","));
+  if (filterHash.length > 0)
+    newHash.push("filters=" + filterHash.join(","));
+  
+  window.location.hash = "#!" + newHash.join("&")
+}
+
+/*
+  Updates filter selections with the values in the location hash.
+*/
+function updateFiltersWithLocationHash() {
+  var currentHash = window.location.hash
+  var hashParts = currentHash.replace("#!", "").split("&")
+  for (var i=0; i < hashParts.length; i++) {
+    if (hashParts[i].length > 0) {      
+      var key = hashParts[i].split("=")[0], stringValue = hashParts[i].split("=")[1]
+      
+      if (key == "filters") {
+        // stringValue looks like "1|true,2|true,3|true"
+        var value = stringValue.split(",")
+        for (var j=0; j < value.length; j++) {
+          var filter_id = value[j].split("|")[0], filter_value = value[j].split("|")[1]
+          $(".filter_checkbox[data-target-filter-id=" + filter_id + "]").attr("checked", true)
+        }
+      }
+      
+      if (key == "stages") {
+        // stringValue looks like "planning,enrolled"
+        for (var j=0; j < stringValue.split(",").length; j++) {
+          $("#stages_selector [data-stage='" + stringValue.split(",")[j] + "']").addClass("selected")
+        }
+      }
+    }
+
+    executeFilters()
   }
 }
