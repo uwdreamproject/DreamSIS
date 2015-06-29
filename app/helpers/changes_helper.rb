335bca15bb3536c6252d9bcbbeff5060b6b2dbd2
Matt Harris <matt@dreamsis.com>
2015-06-28 19:26:36 -0700

    Refactor audit/change logging for rails 3
    Also updates display of the Record History screen to improve output for
    created records and changed records.

--- app/helpers/changes_helper.rb
+++ app/helpers/changes_helper.rb
@@ -1,11 +1,11 @@
 module ChangesHelper
   
   def prep_change_value(val)
-    if val.nil?
-      s = "nil"
+    if val.nil? || val.blank?
+      s = "nothing"
       t = "nil"
     elsif val.is_a?(String)
-      s = "&ldquo;#{h(val)}&rdquo;"
+      s = "&ldquo;".html_safe + h(val) + "&rdquo;".html_safe
       t = "string"
     elsif val.is_a?(Numeric)
       s = val.to_s
@@ -17,11 +17,29 @@ module ChangesHelper
       s = val.to_s
       t = "boolean"
     else
-      s = "&ldquo;#{h(val.to_s)}&rdquo;"
+      s = "&ldquo;".html_safe + h(val.to_s) + "&rdquo;".html_safe
       t = ""
     end
     
     return content_tag(:code, s, :class => "change_value #{t}")
+  end
+  
+  def child_object_link(object, change)
+    anchor_targets = {
+      "CollegeApplication" => "college_applications",
+      "ScholarshipApplication" => "scholarship_applications",
+      "Parent" => "parents",
+      "TestScore" => "test_scores",
+      "CollegeEnrollment" => "college_enrollments",
+      "CollegeDegree" => "college_degrees",
+      "ParticipantMentor" => "mentors",
+      "EventAttendance" => "events",
+      "Document" => "documents",
+      "Note" => "notes"
+    }
+    return change.change_loggable_type.to_s if !object.is_a?(Participant) || anchor_targets[change.change_loggable_type.to_s].nil?
+    anchor = "!/section/#{anchor_targets[change.change_loggable_type]}"
+		link_to change.change_loggable_type.to_s, participant_path(@object, :anchor => anchor)
   end
   
 end
