1658f68e682973d6aece3e52ce0a4d0b56a4cd40
Matt Harris <matt@dreamsis.com>
2015-08-04 21:27:05 -0700

    Add username to rollbar deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -4,7 +4,7 @@ on_app_servers do
   config_file_path = File.join(config.shared_path, "config", "api-keys.yml")
   API_KEYS = YAML.load_file(config_file_path)
   access_token = API_KEYS["rollbar"]["server_side_access_token"]
-  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{access_token} -F environment=#{config.environment} -F revision=#{config.revision}"
+  run "curl https://api.rollbar.com/api/1/deploy/ --silent -F access_token=#{access_token} -F environment=#{config.environment} -F revision=#{config.revision} -F local_username=#{config.deployed_by}"
 end
 
 on_app_servers do
