114333095b2ad7405a62fa87c07d3f8cf072b9f0
Aayush <aayush19saxena@gmail.com>
2015-10-23 16:13:31 -0700

    Final changes for mentor onboarding

--- app/models/people/mentor.rb
+++ app/models/people/mentor.rb
@@ -38,7 +38,7 @@ class Mentor < Person
   # Returns true if +passed_background_check?+ and +signed_risk_form?+ and +currently_enrolled?+ all return true, and
   # if has attended mentor workshop (if necessary)
   def passed_basics?
-    (!Customer.require_background_checks? || (passed_background_check? && passed_sex_offender_check?)) && (!Customer.require_risk_form? || signed_risk_form?) && (!Customer.require_conduct_form? || signed_conduct_form?)&& currently_enrolled? && (!Customer.mentor_workshop_event_type || attended_mentor_workshop?) && (!Customer.require_parental_consent_for_minors? || (is_18? && parental_consent_on_file?))
+    (!Customer.require_background_checks? || (passed_background_check? && passed_sex_offender_check?)) && (!Customer.require_risk_form? || signed_risk_form?) && (!Customer.require_conduct_form? || signed_conduct_form?)&& currently_enrolled? && (!Customer.mentor_workshop_event_type || attended_mentor_workshop?) && (!Customer.require_parental_consent_for_minors? || (is_18? || parental_consent_on_file?))
   end
 
   # Returns a string detailing the steps needed for this mentor
