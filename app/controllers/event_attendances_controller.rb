4d5aed8d4629dbb287e9162f147185b54b4c5504
Mitchell Harper <mitchwharper@gmail.com>
2016-03-28 23:32:42 -0700

    Fix up event attendance authorization logic

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,10 +1,12 @@
 class EventAttendancesController < EventsController
   before_filter :fetch_event
+  before_filter :fetch_attendee, :only => [:edit, :destroy]
   before_filter :declare_audience, :only => [:index, :checkin, :auto_complete_for_person_fullname]
   skip_before_filter :redirect_to_rsvp_if_not_admin
 
-  skip_before_filter :check_authorization, :only => [:index, :checkin, :create, :update, :auto_complete_for_person_fullname]
-  before_filter :check_checkin_authorization, :only => [:index, :checkin, :create, :update, :auto_complete_for_person_fullname]
+  skip_before_filter :check_authorization, :only => [:index, :checkin, :checkin_new_participant, :create, :update, :destroy, :auto_complete_for_person_fullname]
+  before_filter :check_authorization_basic, :only => [:index, :checkin, :checkin_new_participant, :auto_complete_for_person_fullname]
+  before_filter :check_authorization_conditional, :only => [:create, :update, :destroy]
   before_filter :attendee_params, :only => [:create, :update]
 
   protect_from_forgery :only => [:create, :update, :destroy] 
@@ -52,7 +54,6 @@ class EventAttendancesController < EventsController
   end
   
   def edit
-    @attendee = @event.attendees.find(params[:id])
   end
 
   def create
@@ -63,26 +64,7 @@ class EventAttendancesController < EventsController
     upsert
   end
 
-  def upsert
-    @attendee = EventAttendance.where(person_id: attendee_params[:person_id], event_id: @event.id).first_or_create
-    @attendee.update_attributes(attendee_params)
-
-    if @attendee.save
-      flash[:notice] = "Saved"
-    else
-      flash[:error] = "There was an error saving the attendance. Please try again."
-    end
-
-    respond_to do |format|
-      format.html { redirect_to(event_event_attendances_path(@event, :audience => @attendee.try(:person).try(:class))) }
-      format.js   { render 'upsert' }
-      format.json { render :json => @attendee }
-      format.xml  { head :ok }
-    end
-  end
-
   def destroy
-    @attendee = @event.attendees.find(params[:id])
     @attendee.destroy
 
     respond_to do |format|
@@ -121,9 +103,33 @@ class EventAttendancesController < EventsController
   
   
   protected
+
+  def upsert
+    @attendee = @event.attendees.where(person_id: attendee_params[:person_id]).first_or_create
+
+    if @attendee.update_attributes(attendee_params)
+      flash[:notice] = "Saved"
+      status = :ok
+    else
+      flash[:error] = "There was an error saving the attendance. Please try again, or contact an administrator if the issue persists."
+      status = :unprocessable_entity
+    end
+
+    respond_to do |format|
+      format.html { redirect_to(event_event_attendances_path(@event, :audience => @attendee.try(:person).try(:class))) }
+      format.js   { render 'upsert', status: status }
+      format.json { render :json => @attendee, status: status }
+      format.xml  { head status }
+    end
+  end
+
   
   def fetch_event
     @event = Event.find params[:event_id]
+  end
+
+  def fetch_attendee
+    @attendee = @event.attendees.find(params[:id])
   end
   
   def declare_audience
@@ -135,15 +141,25 @@ class EventAttendancesController < EventsController
     end 
   end
   
-  def check_checkin_authorization
-    unless @current_user && (@current_user.admin? || (@current_user.person.try(:respond_to?, :current_lead?) && @current_user.person.try(:current_lead?)))
+  # Allow any fully registered Mentor (or admin) to access
+  def check_authorization_basic
+    unless @current_user && (@current_user.admin? || (@current_user.person.try(:respond_to?, :passed_basics?) && @current_user.person.try(:passed_basics?)))
       render_error("You are not allowed to access that page.")
     end
-    
+  end
+
+  # Mentors and volunteers can check-in Participants and Students, but only privileged users
+  # can check-in Mentors and Volunteers
+  def check_authorization_conditional
+    person = (attendee_params[:person_id] && Person.find(attendee_params[:person_id])) || @attendee.try(:person)
+    if person
+      return check_authorization_basic if person.class == Participant || person.class == Student
+      return check_authorization
+    end
+    render_error("You are not allowed to access that page.")
   end
   
   def attendee_params
     params[:attendee] || params[:event_attendance]
   end
-  
 end
