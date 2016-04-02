35771804d4d593b63c483820730352d226b0e3ac
Matt Harris <matt@dreamsis.com>
2016-04-01 23:05:16 -0700

    Disable SSL fix to work with heroku

--- config/initializers/fix-ssl.rb
+++ config/initializers/fix-ssl.rb
@@ -1,14 +1,14 @@
 require 'open-uri'
 require 'net/https'
 
-module Net
-  class HTTP
-    alias_method :original_use_ssl=, :use_ssl=
-
-    def use_ssl=(flag)
-      self.ca_file = Rails.root.join('config/certs/uwca.pem').to_s
-      self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      self.original_use_ssl = flag
-    end
-  end
-end
+# module Net
+#   class HTTP
+#     alias_method :original_use_ssl=, :use_ssl=
+#
+#     def use_ssl=(flag)
+#       self.ca_file = Rails.root.join('config/certs/uwca.pem').to_s
+#       self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+#       self.original_use_ssl = flag
+#     end
+#   end
+# end
