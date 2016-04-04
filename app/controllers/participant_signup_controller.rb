f0de9162aefe6059c6929dd16e46cbb443ad7854
Aayush <aayush19saxena@gmail.com>
2016-04-03 23:52:19 -0700

    minor changes in indentations

--- app/controllers/participant_signup_controller.rb
+++ app/controllers/participant_signup_controller.rb
@@ -3,22 +3,21 @@ class ParticipantSignupController < ApplicationController
   skip_before_filter :check_authorization	
 
   def index
-  	redirect_to :action => :participant_signup_intake_form
+    redirect_to :action => :participant_signup_intake_form
   end
 
   def intake_form
-	  unless current_user && current_user.person.can_view?(@participant)
+    unless current_user && current_user.person.can_view?(@participant)
       return render_error("You are not allowed to view this form")
     end
-
-	  if request.put?
+    if request.put?
       @participant.intake_form_signature = params[:participant][:intake_form_signature]
       if @participant.update_attributes(params[:participant])
         flash[:notice] = "Your information was successfully updated. Thank you."
       	redirect_to root_url
       end
-	  end
-	end
+    end
+  end
 
   private 
 
--- app/helpers/participant_signup_helper.rb
+++
@@ -1,2 +0,0 @@
-module ParticipantSignupHelper
-end
