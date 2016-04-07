6b5a9d10408501ea25acb28b8f6901f4b2660176
Matt Harris <matt@dreamsis.com>
2016-04-06 21:46:04 -0700

    Update deploy hook for sidekiq

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -8,7 +8,6 @@ on_app_servers do
 end
 
 on_app_servers do
-  sudo "cd #{config.current_path} && bundle exec sidekiqctl stop #{config.shared_path}/pid/DreamSIS_sidekiq.pid"
-  sudo "cd #{config.current_path} && bundle exec sidekiq -d -e #{config.environment} -l #{config.shared_path}/log/DreamSIS_sidekiq.log -P #{config.shared_path}/pid/DreamSIS_sidekiq.pid"
   sudo "chown -R deploy #{config.shared_path}/tmp/"
+  sudo "monit restart all -g DreamSIS_sidekiq"  
 end
