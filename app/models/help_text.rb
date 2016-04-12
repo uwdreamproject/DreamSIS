cc690eeeb9d1a10c80e67608cc144c430b75760a
Matt Harris <matt@dreamsis.com>
2016-04-11 23:41:01 -0700

    Refactor HelpText to always find most scoped result

--- app/models/help_text.rb
+++ app/models/help_text.rb
@@ -6,12 +6,17 @@ class HelpText < ActiveRecord::Base
     HelpText.for_object_class(object_class.to_s, audience)[attribute_name.to_s] 
   end
 
+  # Returns the HelpText for the specified object class, scoped to the requested audience
+  # if possible. To do this, we simply run a query where `audience` matches either the
+  # specified audince or nil. Then we ORDER BY audience, and then populate a hash with the
+  # resulting objects. The nil-audience objects get populated in the Hash first, followed
+  # by the audience-scoped one, so the resultant Hash always returns the most scoped
+  # HelpText object.
   def self.for_object_class(object_class, audience = nil)
-    if audience.nil?
-      Hash[HelpText.where(object_class: object_class.to_s).map{ |ht| [ht.attribute_name.to_s, ht] }]
-    else
-      Hash[HelpText.where(object_class: object_class.to_s, audience: audience.to_s).map{ |ht| [ht.attribute_name.to_s, ht] }]
+    Hash[HelpText
+      .where(object_class: object_class.to_s, audience: [nil, audience.to_s])
+      .order(:audience)
+      .map{ |ht| [ht.attribute_name.to_s, ht] }]
     end
-  end
 
 end
