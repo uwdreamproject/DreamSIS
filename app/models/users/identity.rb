51a47a0767563d76b75f7d80778bfaa86e7ed229
Matt Harris <matt@dreamsis.com>
2016-06-28 19:24:48 -0700

    Remove previous monkey-patch for Identity

--- app/models/users/identity.rb
+++ app/models/users/identity.rb
@@ -2,12 +2,4 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   validates_presence_of :name
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  
-  def self.where(conditions)
-    find :all, conditions: conditions
-  end
-  
-  def persisted?
-    !new_record?
-  end
 end
