9e0369bdc0722c2dee73f6466e83bb0387a1444b
Matt Harris <matt@dreamsis.com>
2016-07-20 22:22:58 -0700

    Style improvements and subheader

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -30,4 +30,59 @@ module BootstrapHelper
     end
   end
   
+  # Prints out breadcrumbs in a Bootstrap friendly way, trying to best-guess each element
+  def breadcrumbs(*args)
+    content_for(:breadcrumbs) do
+      content_tag(:ol, class: "breadcrumb") do
+        for arg in args
+          if arg.is_a?(String)
+            concat content_tag(:li, arg, class: "active")
+          elsif arg.is_a?(Symbol)
+            concat content_tag(:li, link_to(arg.to_s.titleize, arg))
+          elsif arg.is_a?(Array)
+            title = arg.last.is_a?(Symbol) ? arg.last.to_s.titleize : guess_label_text(arg.last)
+            concat content_tag(:li, link_to(title, arg))
+          elsif arg.is_a?(ActiveRecord::Relation)
+            concat(content_tag(:li, class: "dropdown") do
+              concat(content_tag(:a, :class => "dropdown-toggle", "data-toggle" => "dropdown") do
+                concat(guess_label_text( arg.select{|a| current_page?(a) }))
+                concat(" ")
+                concat(content_tag(:span, "", class: "caret"))
+              end)
+              concat(content_tag(:ul, class: "dropdown-menu") do
+                for object in arg
+                  concat(content_tag(:li, link_to(guess_label_text(object), object), class: ("active" if current_page?(object))))
+                end
+              end)
+            end)
+          else
+            concat content_tag(:li, link_to(guess_label_text(arg), arg))
+          end
+        end
+      end
+    end
+  end
+  
+  def operations(options = {}, &block)
+    options = { wrap: true }.merge(options) # wrap in an .btn-group div
+    content_for(:operations) do
+      if block_given?
+        content = capture(&block)
+        options[:wrap] ? content_tag(:div, content, class: "btn-group", role: "group", aria: { label: "Operations Links" }) : content
+      elsif options.is_a?(Array)
+        for link in options
+          case link
+          when :save then concat link_to("Save", "", data: { submit: 'main' }, class: "btn btn-default")
+          end
+        end
+      end
+    end
+  end
+  
+  def guess_label_text(arg)
+    return guess_label_text(arg.last) if arg.is_a?(Array)
+    methods = [:to_label, :fullname, :name, :title]
+    arg.try(methods.find{ |m| arg.respond_to?(m) }) rescue nil
+  end
+
 end
