663aebba290cbd9386fda2f963b018e251a679e5
Matt Harris <matt@dreamsis.com>
2016-08-04 15:25:04 -0700

    Get unit tests working again

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,23 @@
 ENV["Rails.env"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
+
+  def setup
+    Apartment::Tenant.switch!('test-customer')
+    if @request
+      @request.host = 'test-customer.test.host'
+      login_as(:admin)
+    end
+  end
 
   # Add more helper methods to be used by all tests here...
   def login_as(user)
-    @request.session[:user_id] = user ? users(user).id : nil
+    @request.session[:user_id] = users(user).id
   end
   
 end
--- test/unit/helpers/mentor_signup_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MentorSignupHelperTest < ActionView::TestCase
-end
