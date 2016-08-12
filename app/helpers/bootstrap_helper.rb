747c4e5b89663b826886573c67a19746e3a41211
Matt Harris <matt@dreamsis.com>
2016-08-11 22:08:47 -0700

    Style improvements

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -23,7 +23,7 @@ module BootstrapHelper
   # icon will be prepended to the link text.
   def link_to(name = nil, options = nil, html_options = nil, &block)
     if html_options.try(:[], :glyph)
-      name = glyph(html_options.delete(:glyph)) + name
+      name = glyph(html_options.delete(:glyph) + " fa-fw") + name
       super(name, options, html_options, &block)
     else
       super
