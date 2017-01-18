00667ebff190d80cd5bf85baa3d59b8567abac99
Matt Harris <matt@dreamsis.com>
2017-01-18 22:52:14 +0100

    Use strong params

--- app/models/help_text.rb
+++ app/models/help_text.rb
@@ -1,9 +1,9 @@
 class HelpText < ApplicationRecord
-  attr_accessible :attribute_name, :instructions, :object_class, :title, :hint, :audience
+  # TODO attr_accessible :attribute_name, :instructions, :object_class, :title, :hint, :audience
   validates_uniqueness_of :attribute_name, scope: [:object_class, :audience]
   
   def self.for(object_class, attribute_name, audience = nil)
-    HelpText.for_object_class(object_class.to_s, audience)[attribute_name.to_s] 
+    HelpText.for_object_class(object_class.to_s, audience)[attribute_name.to_s]
   end
 
   # Returns the HelpText for the specified object class, scoped to the requested audience
