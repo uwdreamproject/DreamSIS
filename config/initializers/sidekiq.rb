484f48a938431e5d9956b4302d993267194d2e72
Matt Harris <matt@dreamsis.com>
2017-01-19 20:40:09 +0100

    Deployment config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, size: 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, size: 7 }
 end
 
 # Make generic redis available for other purposes
-$redis = Redis::Namespace.new("default", :redis => Redis.new({ url: ENV['REDIS_URL'], network_timeout: 5 }))
+$redis = Redis::Namespace.new("default", :redis => Redis.new({ url: ENV['REDIS_URL'], network_timeout: 5, size: 5 }))
 $redis.client.logger = Rails.logger
