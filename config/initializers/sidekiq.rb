35af6bb9bb6f99d8b87738ff1132cc0f37caf449
Matt Harris <matt@dreamsis.com>
2017-01-19 22:33:34 +0100

    Remove concurrency specifics

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,13 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, size: 5 }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5, size: 5 }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 # Make generic redis available for other purposes
-$redis = Redis::Namespace.new("default", :redis => Redis.new({ url: ENV['REDIS_URL'], network_timeout: 5, size: 5 }))
+$redis = Redis::Namespace.new("default", :redis => Redis.new(
+  { url: ENV['REDIS_URL'] })
+)
 $redis.client.logger = Rails.logger
